369784c5b13fb452bce9eb0cfbbd5451dc69fd0e
RadAndrada <andrada.rad@stud.ubbcluj.ro>
2023-11-27 17:45:58 +0200

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Rad_Andrada_Laborator7
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
