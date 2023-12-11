ccda0a92b7e4fa3b7eeef4c209c2760adab7fa80
RadAndrada <andrada.rad@stud.ubbcluj.ro>
2023-12-11 17:59:02 +0200

    lab10

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -14,9 +14,16 @@ namespace Rad_Andrada_Laborator7
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
+            .ConfigureEssentials(essentials =>
+             {
+                 essentials.UseMapServiceToken("UtTKzyc8ftyYUerAsNMO~OL8i3n_Z7l6mXSx_DMIfRQ~AmR0CyamCFntRN63PRbnOwUCtDyAtUf3hkdYNhHlNCzgd3DJDottAid5kMCGjYdt");
+             });
 
+            return builder.Build();
+        }
+    }
 #if DEBUG
-    		builder.Logging.AddDebug();
+    builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
